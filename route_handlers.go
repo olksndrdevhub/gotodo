7eea5ad0299da2d57224ba376bccd74eb51b8d37
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2023-10-06 16:48:51 +0300

    messing with templates

--- route_handlers.go
+++ route_handlers.go
@@ -10,11 +10,11 @@ import (
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	log.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
-	template, err := template.ParseFiles("templates/base.html", "templates/partials/_header.html")
+	template, err := template.ParseFiles("templates/base.html", "templates/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = template.Execute(w, nil)
+	err = template.ExecuteTemplate(w, "base.html", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,11 +22,11 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	log.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
-	template, err := template.ParseFiles("templates/base.html")
+	template, err := template.ParseFiles("templates/base.html", "templates/hi.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = template.Execute(w, ps.ByName("name"))
+	err = template.ExecuteTemplate(w, "base.html", ps.ByName("name"))
 	if err != nil {
 		log.Fatal(err)
 	}
